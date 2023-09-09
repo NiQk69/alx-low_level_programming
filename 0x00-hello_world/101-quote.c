1b729b7e6f4d72f2bc7c2e7e430437a1ab3088f7
Owusu Nicholas <ynick8846@gmail.com>
2023-09-09 14:49:32 -0700

    Printing a standard output without using printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 int main(void)
-{
-	char *str = "and that piece  of art is useful\" -- Dora Korpar, 2015-10-19\n";
-    write(STDERR_FILENO, str, 59);
-    return 1;
+{char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, str, 59);
+return (1);
 }
