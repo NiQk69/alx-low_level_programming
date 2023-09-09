740ff83287562b56524ee711b02369c48c93cf13
Owusu Nicholas <ynick8846@gmail.com>
2023-09-08 18:35:40 -0700

    Listing the size of various types of data in C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(void)
 {
-	printf("Size of a char: %lu Byte(s)\n", sizeof(char));
-	printf("size of an int: %lu Byte(s)\n", sizeof(int));
-	printf("size of a long int: %lu Byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %lu Byte(s)\n", sizeof(long long int));
-	printf("size of a float: %lu Byte(s)\n", sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("size of an int: %lu byte(s)\n", sizeof(int));
+	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("size of a float: %lu byte(s)\n", sizeof(float));
 return (0) ; }
