6660917752ff4fec6ab1e4011c1445608f65364e
Owusu Nicholas <ynick8846@gmail.com>
2023-09-14 22:50:50 +0000

    Finding the number of days left in the year when given a particular day

---
+++ 0x03-debugging/3-print_remaining_days.c
@@ -0,0 +1,39 @@
+nclude <stdio.h>
+#include "main.h"
+
+/**
+ *  * print_remaining_days - takes a date and prints how many days are
+ *   * left in the year, taking leap years into account
+ *    * @month: month in number format
+ *     * @day: day of month
+ *      * @year: year
+ *       * Return: void
+ *        */
+
+void print_remaining_days(int month, int day, int year)
+{
+	    int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+	        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+	        {
+		     // Leap year
+		     days_in_month[2] = 29;
+                }
+					   
+	       if (month < 1 || month > 12 || day < 1 || day > days_in_month[month])
+               {
+	            // Invalid date
+		    printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+	       }
+	      else
+	       {         
+                   int day_of_year = day;
+		   for (int i = 1; i < month; i++)
+	           {      day_of_year += days_in_month[i];
+		  
+		   }
+					    
+		   printf("Day of the year: %d\n", day_of_year);
+		   printf("Remaining days: %d\n", 365 + ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) - day_of_year);
+	      }   
+}				 
