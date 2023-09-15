39a5d60ecbeaf48ed10b91d6d704fda75239d674
Owusu Nicholas <ynick8846@gmail.com>
2023-09-15 06:04:00 +0000

    Checking a letter to verify if its an upper case or not

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,25 +1,25 @@
-#include "main.h"
+nclude "main.h"
 
 int _putchar(char c);
 
 int main(void)
 {
-	    char c;
+    char c;
 
-	        c = 'A';
-		    _putchar(c);
-		        _putchar(':');
-			    _putchar(' ');
-			        _putchar(_isupper(c) + '0');
-				    _putchar('\n');
+    c = 'A';
+    _putchar(c);
+    _putchar(':');
+    _putchar(' ');
+    _putchar(_isupper(c) + '0');
+    _putchar('\n');
 
-				        c = 'a';
-					    _putchar(c);
-					        _putchar(':');
-						    _putchar(' ');
-						        _putchar(_isupper(c) + '0');
-							    _putchar('\n');
+    c = 'a';
+    _putchar(c);
+    _putchar(':');
+    _putchar(' ');
+    _putchar(_isupper(c) + '0');
+    _putchar('\n');
 
-							        return (0);
+    return (0);
 }
 
